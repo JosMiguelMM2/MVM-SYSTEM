8bf3e9b7b66730de408e5e3b203d638d57a5576e
josmiguelmm <miguelmendezm0802@gmail.com>
2022-10-19 01:27:26 -0500

    Creando front-end

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -4,13 +4,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//import { AppRoutingModule } from './app-routing.module';
+import { HttpModule, } from '@angular/http';
+import { HttpClientModule, } from '@angular/common/http';
+import { RouterModule, Routes} from '@angular/router';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { MenuInicioComponent } from './menu-inicio/menu-inicio.component';
+
+
+const appRoutes: Routes = [
+  { path: 'menu-inicio', 
+  component: MenuInicioComponent },
+  { path: '',
+    redirectTo: '/menu-inicio',
+    pathMatch: 'full'
+  }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuInicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    HttpModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
